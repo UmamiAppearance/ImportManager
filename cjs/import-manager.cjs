0243bb7f781722ab939fbc1387313aa9db148611
UmamiAppearance <mail@umamiappearance.eu>
2023-02-24 09:41:11 +0100

    dependency updates

--- cjs/import-manager.cjs
+++ cjs/import-manager.cjs
@@ -379,7 +379,7 @@ class ImportManagerUnitMethods {
  * It handles code analysis, creates units from import
  * statements, attaches methods to the units and more.
  * 
- * @version 0.3.0
+ * @version 0.3.1
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  * @see https://github.com/UmamiAppearance/rollup-plugin-import-manager
